3a6ed0fbc3eb4d25e38ddfefc7e7d3beaa01d9e9
vikShevchenko <63931417+vikShevchenko@users.noreply.github.com>
2022-11-19 18:02:35 +0200

    test

--- src/index.js
+++ src/index.js
@@ -25,6 +25,22 @@ refs.inputForm.addEventListener(
 );
 
 //fetchCountries;
+function fetchCountries(name) {
+  fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then(name => {
+      if (!name.ok) {
+        throw new Error(name.statusText);
+      }
+
+      return name.json();
+    })
+    .then(data => { console.log(data);
+
+    const marcup = countryCard(data);
+
+    });
+}  
+
 
 
 
